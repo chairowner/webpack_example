adbb64bcedf6dc9c5be38ef846f4e0f4b966cf84
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-02-01 20:06:42 +0500

    init

---
+++ config/build/types/types.ts
@@ -0,0 +1,18 @@
+export interface BuildPaths {
+	entry: string;
+	html: string;
+	output: string;
+	public: string;
+	src: string;
+}
+
+export type BuildMode = "production" | "development";
+export type BuildPlatform = "mobile" | "desktop";
+
+export interface BuildOptions {
+	port: number;
+	paths: BuildPaths;
+	mode: BuildMode;
+	platform: BuildPlatform;
+	bundleAnalyze?: boolean;
+}
