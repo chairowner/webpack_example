81e66092e37b8ef3027d929d299c73241c895d7b
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-03-28 23:39:08 +0500

    add normalize file for styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import "@/styles/normalize.scss";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
